c2d05b017417ce936a623569e8d2456d8f20c6b5
재훈 이 <jh051777@naver.com>
2024-12-30 14:52:38 +0900

    이거야 민수얌

--- FFmpeg_Form/FFmpeg_Form/StreamViewer.h
+++ FFmpeg_Form/FFmpeg_Form/StreamViewer.h
@@ -23,6 +23,7 @@ namespace FFmpegForm {
 	using namespace System::Drawing;
 	using namespace System::Threading;
 
+	AVDictionary* options = nullptr;
 	AVFormatContext* fmt_ctx = nullptr;
 	AVCodecContext* vcodec_ctx = nullptr;
 	AVCodecContext* acodec_ctx = nullptr;
@@ -30,17 +31,18 @@ namespace FFmpegForm {
 	const AVCodec* acodec = nullptr;
 	AVPacket* pkt = nullptr;
 	AVPixelFormat pix_fmt;
-	AVFrame* vframe = av_frame_alloc();
-	AVFrame* rgb_frame = av_frame_alloc();
+	AVFrame* vframe = nullptr;
+	AVFrame* rgb_frame = nullptr;
+	AVFrame* screenshot_frame = nullptr;
 	SwsContext* sws_ctx = nullptr;
+	SwsContext* screenshot_ctx = nullptr;
 	std::queue<AVFrame*> frame_q;
 	std::mutex q_mutex;
 	volatile bool isStreaming;
 	char* url;
-	
+		
 	int frame_count;
-	
-	
+	std::atomic<int> second_counter(0);
 
 	/// <summary>
 	/// StreamViewer에 대한 요약입니다.
@@ -48,10 +50,13 @@ namespace FFmpegForm {
 	public ref class StreamViewer : public System::Windows::Forms::Form
 	{
 		DateTime last_fps_time;
-		
+		Bitmap^ screenshot_bitmap = nullptr;
 		int media_width = 0;
 		int media_height = 0;
 		int vst_idx = -1;
+		Thread^ read_thread;
+		Thread^ draw_thread;
+		Thread^ time_thread;
 		
 	public:
 		StreamViewer(void)
@@ -81,14 +86,13 @@ namespace FFmpegForm {
 
 		// 파일 열기
 		void open_file(Object^ sender, EventArgs^ e) {
-			Exit_media(sender, e);
+			this->Invoke(gcnew EventHandler(this, &StreamViewer::Exit_media));
 			OpenFile^ open_file = gcnew OpenFile();
 			if (open_file->ShowDialog() == System::Windows::Forms::DialogResult::OK) {
-				Stop_stream();
+				//Stop_stream();
 				String^ input_str = open_file->input_url;
 				IntPtr ptrToNative = System::Runtime::InteropServices::Marshal::StringToHGlobalAnsi(input_str);
 				url = static_cast<char*>(ptrToNative.ToPointer());
-				//Start_stream();
 				Receive_stream();
 			};
 		}
@@ -114,20 +118,43 @@ namespace FFmpegForm {
 		void Receive_stream() {
 			avformat_network_init();
 
-			//av_log_set_level(AV_LOG_DEBUG);
+			av_dict_set(&options, "timeout", "5000000", 0);
 
-			if (avformat_open_input(&fmt_ctx, url, NULL, NULL) < 0) {
-				ShowError("Failed to open stream.");
+			//fmt_ctx = avformat_alloc_context();
+			//av_log_set_level(AV_LOG_DEBUG);
+			int res = avformat_open_input(&fmt_ctx, url, NULL, &options);
+			av_dict_free(&options);
+			if (res < 0) {
+				char err_buf[AV_ERROR_MAX_STRING_SIZE] = { 0 };
+				av_strerror(res, err_buf, sizeof(err_buf));
+				std::cerr << "Error opening input: " << err_buf << std::endl;
 				rtb_log->Text += gcnew String("url을 열 수 없습니다") + Environment::NewLine;
 				return;
 			}
 
-			if (avformat_find_stream_info(fmt_ctx, NULL) < 0) {
-				ShowError("Failed to receive stream info.");
+			if (res == AVERROR(EIO)) {
+				std::cerr << "I/O Error" << std::endl;
+				return;
+			}
+			else if (res == AVERROR(EINVAL)) {
+				std::cerr << "Invalid URL or unsupported format." << std::endl;
+				return;
+			}
+			else if (res == AVERROR(ENOMEM)) {
+				std::cerr << "Memory allocation error." << std::endl;
+				return;
+			}
+
+			res = avformat_find_stream_info(fmt_ctx, NULL);
+
+			if (res < 0) {
+				char err_buf[AV_ERROR_MAX_STRING_SIZE] = { 0 };
+				av_strerror(res, err_buf, sizeof(err_buf));
+				std::cerr << "Error finding stream info: " << err_buf << std::endl;
 				avformat_close_input(&fmt_ctx);
 				return;
 			}
-			
+
 			for (unsigned i = 0; i < fmt_ctx->nb_streams; i++) {
 				if (fmt_ctx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
 					vst_idx = i;
@@ -177,21 +204,13 @@ namespace FFmpegForm {
 			if (isStreaming) return;
 
 			isStreaming = true;
-
-			Thread^ read_thread = gcnew Thread(gcnew ThreadStart(this, &StreamViewer::read_frame));
-			Thread^ draw_thread = gcnew Thread(gcnew ThreadStart(this, &StreamViewer::draw_frame));
+			read_thread = gcnew Thread(gcnew ThreadStart(this, &StreamViewer::read_frame));
+			draw_thread = gcnew Thread(gcnew ThreadStart(this, &StreamViewer::draw_frame));
 			read_thread->IsBackground = true;
 			draw_thread->IsBackground = true;
 			read_thread->Start();
 			draw_thread->Start();
 		}
-		/*ref class AVFrameWrapper {
-		public:
-			AVFrame* Frame;
-			AVFrameWrapper(AVFrame* frame) {
-				Frame = frame;
-			}
-		};*/
 
 		// 프레임 읽기 // 쓰레드 시작
 		void read_frame() {
@@ -243,6 +262,7 @@ namespace FFmpegForm {
 			avformat_close_input(&fmt_ctx);
 
 			this->Invoke(gcnew Action(this, &StreamViewer::Stop_stream));
+			read_thread->Join();
 
 		}
 
@@ -280,17 +300,27 @@ namespace FFmpegForm {
 		}
 
 		void Update_pb_bitmap() {
-			//if (vcodec_ctx == nullptr || rgb_frame == nullptr) return;
+			if (vcodec_ctx == nullptr || rgb_frame == nullptr || screenshot_frame == nullptr) {
+				std::cerr << "vcodec_ctx or rgb_frame == nullptr" << std::endl;
+				return;
+			}
 
 			// 기존 sws_ctx 해제 및 재설정
-			if (sws_ctx) {
+			if (sws_ctx || screenshot_ctx) {
 				sws_freeContext(sws_ctx);
+				sws_freeContext(screenshot_ctx);
 				sws_ctx = nullptr;
+				screenshot_ctx = nullptr;
 			}
 			
 			sws_ctx = sws_getContext(
 				vcodec_ctx->width, vcodec_ctx->height, pix_fmt,
 				media_width, media_height, AV_PIX_FMT_BGR24,
+				SWS_BILINEAR, NULL, NULL, NULL
+			);
+			screenshot_ctx = sws_getContext(
+				vcodec_ctx->width, vcodec_ctx->height, pix_fmt,
+				vcodec_ctx->width, vcodec_ctx->height, AV_PIX_FMT_BGR24,
 				SWS_BILINEAR, NULL, NULL, NULL
 			);
 
@@ -301,11 +331,14 @@ namespace FFmpegForm {
 
 			// RGB 버퍼 할당
 			static uint8_t* rgb_buf = nullptr;
+			static uint8_t* screenshot_buf = nullptr;
 			static int buf_size = 0;
+			int screenshot_buf_size = av_image_get_buffer_size(AV_PIX_FMT_BGR24, vcodec_ctx->width, vcodec_ctx->height, 1);
 			int new_buf_size = av_image_get_buffer_size(AV_PIX_FMT_BGR24, media_width, media_height, 1);
 			if (!rgb_buf || buf_size != new_buf_size)
 			{
 				rgb_buf = (uint8_t*)av_malloc(new_buf_size);
+				screenshot_buf = (uint8_t*)av_malloc(screenshot_buf_size);
 				buf_size = new_buf_size;
 				if (!rgb_buf) {
 					ShowError("Failed to allocate RGB buffer.");
@@ -315,9 +348,12 @@ namespace FFmpegForm {
 
 			// rgb_frame 할당 및 데이터 배열 초기화
 			av_frame_free(&rgb_frame);
+			av_frame_free(&screenshot_frame);
 			rgb_frame = av_frame_alloc();
+			screenshot_frame = av_frame_alloc();
 			//av_frame_unref(rgb_frame);
 			av_image_fill_arrays(rgb_frame->data, rgb_frame->linesize, rgb_buf, AV_PIX_FMT_BGR24, media_width, media_height, 1); // 종료 후 다시 스트리밍 시 에러
+			av_image_fill_arrays(screenshot_frame->data, screenshot_frame->linesize, screenshot_buf, AV_PIX_FMT_BGR24, vcodec_ctx->width, vcodec_ctx->height, 1);
 
 			//Console::WriteLine("Updated sws context and RGB buffer for new PictureBox size.");
 		}
@@ -365,18 +401,24 @@ namespace FFmpegForm {
 
 		// 비트맵 이미지 그리기 // 쓰레드 시작
 		void draw_frame() {
+			//time_thread = gcnew Thread(gcnew ThreadStart(this, &StreamViewer::play_time));
+			//time_thread->IsBackground = true;
+			//time_thread->Start();
 			//bool enough_q = false;
 			const int target_fps = 33;
 			const auto target_frame_duration = std::chrono::milliseconds(1000 / target_fps);
 			this->Invoke(gcnew EventHandler(this, &StreamViewer::form_resize));
 
+			screenshot_frame = av_frame_alloc();
 			rgb_frame = av_frame_alloc();
+			sws_ctx = sws_alloc_context();
+			screenshot_ctx = sws_alloc_context();
 
 			while (isStreaming) {
 				auto frame_start_time = std::chrono::steady_clock::now();
 
-				if (frame_q.size() > 100) {
-					while (frame_q.size() > 20) {
+				if (frame_q.size() > 40) {
+					while (frame_q.size() > 10) {
 						AVFrame* d_frame = frame_q.front();
 						frame_q.pop();
 						av_frame_free(&d_frame);
@@ -384,38 +426,14 @@ namespace FFmpegForm {
 					Console::WriteLine("Clear Queue count: " + frame_q.size());
 				}
 
-				if (frame_q.size() > 20) {
+				if (frame_q.size() > 10) {
 					AVFrame* frame = Dequeue_frame();
 					if (!frame || !frame->data[0]) {
 						av_frame_unref(frame);
 						continue;
 					}
 
-					//try {
-						//if (sws_ctx == NULL) {
-						//	sws_ctx = sws_getContext(
-						//		frame->width, frame->height, vcodec_ctx->pix_fmt,
-						//		pb_media->Width, pb_media->Height, AV_PIX_FMT_BGR24,
-						//		SWS_BILINEAR, NULL, NULL, NULL
-						//	);
-						//	if (sws_ctx == NULL) {
-						//		ShowError("Error initializing sws context.");
-						//		av_frame_free(&frame);
-						//		continue;
-						//	}
-						//	int buf_size = av_image_get_buffer_size(AV_PIX_FMT_BGR24, frame->width, frame->height, 1);
-						//	uint8_t* rgb_buf = (uint8_t*)av_malloc(buf_size);
-						//	if (!rgb_buf)
-						//	{
-						//		ShowError("Failed to allocate RGB buffer.");
-						//		av_frame_free(&frame);
-						//		continue;
-						//	}
-						//	/*av_frame_free(&rgb_frame);
-						//	rgb_frame = av_frame_alloc();*/
-						//	av_image_fill_arrays(rgb_frame->data, rgb_frame->linesize, rgb_buf, AV_PIX_FMT_BGR24, frame->width, frame->height, 1);
-						//}
-					if (!sws_ctx) {
+					if (sws_ctx == nullptr) {
 						Console::WriteLine("sws_ctx is null, skipping frame.");
 						av_frame_free(&frame);
 						continue;
@@ -423,43 +441,30 @@ namespace FFmpegForm {
 					this->Invoke(gcnew Action(this, &StreamViewer::Update_pb_bitmap));
 					
 					sws_scale(sws_ctx, frame->data, frame->linesize, 0, frame->height, rgb_frame->data, rgb_frame->linesize);
-					
+					sws_scale(screenshot_ctx, frame->data, frame->linesize, 0, frame->height, screenshot_frame->data, screenshot_frame->linesize);
 					//Console::WriteLine("##########sws_scale executed successfully.#############");
 					Bitmap^ bitmap = nullptr;
+					
 					try {
 
 						bitmap = gcnew Bitmap(
 							media_width, media_height, rgb_frame->linesize[0],
 							Imaging::PixelFormat::Format24bppRgb, IntPtr(rgb_frame->data[0])
 						);
-						
+						screenshot_bitmap = gcnew Bitmap(
+							vcodec_ctx->width, vcodec_ctx->height, screenshot_frame->linesize[0],
+							Imaging::PixelFormat::Format24bppRgb, IntPtr(screenshot_frame->data[0])
+						);
 					}
 					catch (System::Exception^ ex) {
 						Console::WriteLine("Bitmap update error: " + ex->Message);
 						av_frame_free(&frame);
 					}
 					
-						pb_media->BeginInvoke(gcnew Action<Bitmap^>(this, &StreamViewer::Update_pb), bitmap);
-					
-					/*catch (System::ArgumentException^ ex) {
-						Console::WriteLine("Bitmap creation error: " + ex->Message);
-						av_frame_free(&frame);
-						continue;
-					}
-					catch (Exception^ ex)
-					{
-						Console::WriteLine("General exception: " + ex->Message);
-						av_frame_free(&frame);
-						continue;
-					}
-					catch (...) {
-						Console::WriteLine("Unknown exception caught during frame processing.");
-						av_frame_free(&frame);
-						continue;
-					}*/
+					pb_media->BeginInvoke(gcnew Action<Bitmap^>(this, &StreamViewer::Update_pb), bitmap);
+
 					av_frame_free(&frame);
 				}
-				
 
 				auto frame_end_time = std::chrono::steady_clock::now();
 				auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(frame_end_time - frame_start_time);
@@ -468,16 +473,43 @@ namespace FFmpegForm {
 				}
 			}
 			sws_freeContext(sws_ctx);
+			sws_freeContext(screenshot_ctx);
 			sws_ctx = nullptr;
+			screenshot_ctx = nullptr;
 
-			if (rgb_frame) {
-				av_freep(&rgb_frame->data[0]);
+			if (rgb_frame || screenshot_frame) {
+				//av_freep(&rgb_frame->data[0]);
 				av_frame_free(&rgb_frame);
+				av_frame_free(&screenshot_frame);
 			}
+			
 			this->Invoke(gcnew Action(this, &StreamViewer::Stop_stream));
+			draw_thread->Join();
 		}
 
+
+		void play_time() {
+			auto play_start_time = std::chrono::steady_clock::now();
+			while (isStreaming) {
+				auto play_current_time = std::chrono::steady_clock::now();
+				auto elapsed_time = std::chrono::duration_cast<std::chrono::seconds>(play_current_time - play_start_time);
+				if (elapsed_time.count() >= (second_counter.load() + 1)) {
+					second_counter++;
+					int total_seconds = second_counter.load();
+					int minutes = total_seconds / 60;
+					int seconds = total_seconds % 60;
+					
+					//this->Invoke(gcnew Action <int, int>(&StreamViewer::set_timer) , minutes, seconds);
+				}
+				std::this_thread::sleep_for(std::chrono::milliseconds(10));
+			}
+			second_counter = 0;
+			time_thread->Join();
+		}
 		
+		void set_timer(int minutes, int seconds) {
+			lbl_time->Text = String::Format("{0:D2} : {1:D2}", minutes, seconds);
+		}
 
 		// 스트림 정보 출력
 		void Update_log(String^ codec_info) {
@@ -495,7 +527,7 @@ namespace FFmpegForm {
 		void Screenshot(Object^ sender, EventArgs^ e) {
 			DateTime^ date = DateTime::Now;
 			String^ name = date->ToString("yyyy-MM-dd-HH.mm.ss");
-			pb_media->Image->Save("C:/Users/kimss/Desktop/MediaPlayer Screenshot/" + name + ".jpg", Imaging::ImageFormat::Jpeg);
+			screenshot_bitmap->Save("C:/Users/kimss/Desktop/MediaPlayer Screenshot/" + name + ".jpg", Imaging::ImageFormat::Jpeg);
 			MessageBox::Show("스크린샷 완료");
 		}
 
@@ -503,7 +535,7 @@ namespace FFmpegForm {
 		void Exit_media(Object^ sender, EventArgs^ e) {
 			Stop_stream();
 			clearTimer = gcnew System::Windows::Forms::Timer();
-			clearTimer->Interval = 500;
+			clearTimer->Interval = 100;
 
 			clearTimer->Tick += gcnew EventHandler(this, &StreamViewer::remove_pb);
 
@@ -518,6 +550,7 @@ namespace FFmpegForm {
 			pb_media->Invalidate();
 			rtb_log->Clear();
 			lbl_fps->Text = "FPS:";
+			lbl_time->Text = "00 : 00";
 		}
 
 		
@@ -875,7 +908,6 @@ protected:
 			this->tlp_main->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent, 17.34435F)));
 			this->tlp_main->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent, 82.65565F)));
 			this->tlp_main->Controls->Add(this->tlp_left, 0, 0);
-			this->tlp_main->Controls->Add(this->lbl_time, 1, 1);
 			this->tlp_main->Controls->Add(this->tlp_media, 1, 0);
 			this->tlp_main->Dock = System::Windows::Forms::DockStyle::Fill;
 			this->tlp_main->Location = System::Drawing::Point(0, 0);
@@ -892,6 +924,7 @@ protected:
 			this->tlp_left->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent, 100)));
 			this->tlp_left->Controls->Add(this->btn_open, 0, 0);
 			this->tlp_left->Controls->Add(this->lbl_fps, 0, 1);
+			this->tlp_left->Controls->Add(this->lbl_time, 0, 3);
 			this->tlp_left->Controls->Add(this->rtb_log, 0, 2);
 			this->tlp_left->Dock = System::Windows::Forms::DockStyle::Fill;
 			this->tlp_left->Location = System::Drawing::Point(3, 3);
@@ -949,14 +982,14 @@ protected:
 			// lbl_time
 			// 
 			this->lbl_time->AutoSize = true;
-			this->lbl_time->Dock = System::Windows::Forms::DockStyle::Left;
-			this->lbl_time->Font = (gcnew System::Drawing::Font(L"맑은 고딕", 10.125F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->lbl_time->Dock = System::Windows::Forms::DockStyle::Right;
+			this->lbl_time->Font = (gcnew System::Drawing::Font(L"맑은 고딕", 16.125F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(129)));
 			this->lbl_time->ForeColor = System::Drawing::Color::White;
 			this->lbl_time->ImageAlign = System::Drawing::ContentAlignment::MiddleLeft;
-			this->lbl_time->Location = System::Drawing::Point(276, 732);
+			this->lbl_time->Location = System::Drawing::Point(81, 646);
 			this->lbl_time->Name = L"lbl_time";
-			this->lbl_time->Size = System::Drawing::Size(106, 97);
+			this->lbl_time->Size = System::Drawing::Size(166, 80);
 			this->lbl_time->TabIndex = 2;
 			this->lbl_time->Text = L"00 : 00";
 			// 
@@ -1029,7 +1062,6 @@ protected:
 			this->Text = L"미디어 플레이어";
 			this->Resize += gcnew System::EventHandler(this, &StreamViewer::form_resize);
 			this->tlp_main->ResumeLayout(false);
-			this->tlp_main->PerformLayout();
 			this->tlp_left->ResumeLayout(false);
 			this->tlp_left->PerformLayout();
 			this->tlp_media->ResumeLayout(false);
@@ -1040,7 +1072,8 @@ protected:
 		}
 
 #pragma endregion
-	};
+
+};
 	[STAThread]
 		int main(array<System::String^>^ args) {
 		Application::EnableVisualStyles();
